1695f0fe23fc1abce4b7777adcbf831752b36ed3
Andrey Pankov <30828361+NordMad@users.noreply.github.com>
2017-09-06 18:59:00 +0500

    Sedona Final

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var toggleButton = document.querySelector(".toggle-button");
+var modalSearch = document.querySelector(".modal-search");
+
+toggleButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalSearch.classList.toggle("modal-toggle");
+});
